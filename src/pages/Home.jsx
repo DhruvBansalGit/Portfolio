f39caf7c35fc7dfa6ce5b7cf818c97bbfe2ccd3d
Dhruv <bansaldhruv03@example.com>
2024-05-09 15:35:14 +0530

    autoplay commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     return ()=>{
       audioRef.current.pause()
     }
-  }, [isPlayMusic])
+  }, [isPlayMusic,isRotating])
   
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
